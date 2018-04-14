a7b3531b46031324db0432d8f7629b43949ece5b
Anwesh43 <anweshthecool0@gmail.com>
2018-04-14 08:54:00 +0530

    adding LineInCircle

--- lineincircleview/src/main/java/ui/anwesome/com/lineincircleview/LineInCircleView.kt
+++ lineincircleview/src/main/java/ui/anwesome/com/lineincircleview/LineInCircleView.kt
@@ -73,4 +73,41 @@ class LineInCircleView (ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LineInCircle (var i : Int, private val state : State = State()) {
+        fun draw(canvas : Canvas, paint : Paint) {
+            paint.color = Color.WHITE
+            val w : Float = canvas.width.toFloat()
+            val h : Float = canvas.height.toFloat()
+            val r : Float = Math.min(w, h)/3
+            paint.strokeWidth = Math.min(w, h)/50
+            paint.style = Paint.Style.STROKE
+            canvas.save()
+            canvas.translate(w/2, h/2)
+            canvas.drawCircle(0f, 0f, r, paint)
+            val updated_deg = 180 * state.scale
+            val path = Path()
+            for (i in 0..1) {
+                val deg = 90f + (1 - 2 * i) * updated_deg
+                val x : Float = r * Math.cos(deg * Math.PI/180).toFloat()
+                val y : Float = r * Math.sin(deg * Math.PI/180).toFloat()
+                if (i == 0) {
+                    path.moveTo(x, y)
+                }
+                else {
+                    path.lineTo(x, y)
+                }
+            }
+            canvas.drawPath(path, paint)
+            canvas.restore()
+        }
+
+        fun update(stopcb : (Float) -> Unit) {
+            state.update(stopcb)
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            state.startUpdating(startcb)
+        }
+    }
 }
